1856d55086edf89d9357d846a24ccfd4da4fc946
Thiago Rondon <thiago.rondon@gmail.com>
2012-10-30 16:22:20 -0200

    add FonteRecurso
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

---
+++ lib/POFOMD/Schema/Result/Recurso.pm
@@ -0,0 +1,34 @@
+
+package POFOMD::Schema::Result::Recurso;
+
+use strict;
+use warnings;
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->load_components(qw(Core));
+
+__PACKAGE__->table('recurso');
+__PACKAGE__->add_columns(
+    'id' => {
+        'data_type'         => 'integer',
+        'is_auto_increment' => 1,
+        is_nullable         => 0,
+        sequence            => "funcao_id_seq",
+    },
+    'codigo' => { 'data_type' => 'varchar' },
+    'nome'   => { 'data_type' => 'varchar' }
+);
+
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->add_unique_constraint( [ qw/codigo/ ] );
+
+__PACKAGE__->has_many(
+    'gastos',
+    'POFOMD::Schema::Result::Gasto',
+    { "foreign.funcao_id" => "self.id" },
+    { cascade_copy        => 0, cascade_delete => 0 },
+);
+
+1;
+
