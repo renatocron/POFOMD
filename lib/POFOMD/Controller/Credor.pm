0ad7b5bc5a140d7bc29b957fa4c01a39b3ea4a25
Thiago Rondon <thiago.rondon@gmail.com>
2012-12-11 21:15:50 -0200

    páginação
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- lib/POFOMD/Controller/Credor.pm
+++ lib/POFOMD/Controller/Credor.pm
@@ -9,13 +9,20 @@ use POFOMD::Utils qw(formata_real formata_valor formata_float bgcolor);
 sub base : Chained('/base') : PathPart('credor') : CaptureArgs(1) {
     my ( $self, $c, $uri ) = @_;
     my $ds = $c->model('DB::Beneficiario');
-    my $ds_obj = $ds->find({ uri => $uri });
+    my $ds_obj = $ds->find( { uri => $uri } );
     $c->detach if !$ds_obj;
     $c->stash->{credor} = $ds_obj;
 }
 
 sub root : Chained('base') PathPart('') Args(0) {
     my ( $self, $c ) = @_;
+
+    my $page = $c->req->param('page') || 1;
+    $c->stash->{gastos} =
+      $c->model('DB::Gasto')->search( { beneficiario_id => $c->stash->{credor}->id },
+        { rows => 50, page => $page } );
+
+    $c->stash->{page} = $page;
 }
 
 __PACKAGE__->meta->make_immutable;
